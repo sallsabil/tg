75a24bd898dd87196580e77aa4cf2366de75cea2
sallsabil <sallsabill@gmail.com>
2017-03-02 20:46:27 +0330

    Update json-tg.c

--- json-tg.c
+++ json-tg.c
@@ -484,7 +484,9 @@ json_t *json_pack_message (struct tgl_message *M) {
   assert (json_object_set (res, "id", json_string (print_permanent_msg_id (M->permanent_id))) >= 0);
   if (!(M->flags & TGLMF_CREATED)) { return res; }
 
-  assert (json_object_set (res, "flags", json_integer (M->flags)) >= 0);
+    assert (json_object_set (res, "msgid", json_string ("546541")) >= 0);
+	
+    assert (json_object_set (res, "flags", json_integer (M->flags)) >= 0);
  
   if (tgl_get_peer_type (M->fwd_from_id)) {
     assert (json_object_set (res, "fwd_from", json_pack_peer (M->fwd_from_id)) >= 0);
